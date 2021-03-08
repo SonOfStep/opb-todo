ee4bf85fe263cf02007e25b51424f3a9d1d8b781
Omar Nurmakhanov <omarionurmsad@gmail.com>
2021-03-08 21:56:34 +0500

    Add reducer and form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
+
+  const initialState = {
+    name: "",
+    text: "",
+    todoList: [],
+  }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "change-name":
+        return {
+          ...state,
+          name: action.payload
+        }
+
+      case "change-text":
+        return {
+          ...state,
+          text: action.payload
+        }
+
+      case "clear-state":
+        return {
+          name: '',
+          text: ''
+        }
+
+      default:
+        throw new Error("Unknown action")
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    alert("Отправлено сообщение" + state.text + "\n От - " + state.name)
+
+    dispatch({ type: "clear-state" })
+  }
+
+  const handleChange = event => {
+    dispatch({ type: "change-" + event.target.name, payload: event.target.value })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        <label>Имя:
+          <input type="text" name="name" value={state.name} onChange={handleChange} />
+        </label>
+
+        <label>Текст:
+          <input type="text" name="text" value={state.text} onChange={handleChange} />
+        </label>
+
+        <button type="submit" >Отправить</button>
+
+      </form>
+
     </div>
   );
 }
