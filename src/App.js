b283e446d0cb7a1334888df51bdc88befbdab142
Omar Nurmakhanov <omarionurmsad@gmail.com>
2021-03-11 06:32:04 +0500

    add save state to loacalStorage

--- src/App.js
+++ src/App.js
@@ -1,70 +1,49 @@
-import { useReducer } from 'react';
+import { useEffect, useMemo, useReducer } from 'react';
+import Context from './Context'
 import './App.css';
+import TodoList from './components/TodoList';
+import reducer from './reducer';
+import TodoCreate from './components/TodoCreate';
 
 function App() {
 
-  const initialState = {
-    name: "",
-    text: "",
-    todoList: [],
-  }
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "change-name":
-        return {
-          ...state,
-          name: action.payload
-        }
-
-      case "change-text":
-        return {
-          ...state,
-          text: action.payload
-        }
-
-      case "clear-state":
-        return {
-          name: '',
-          text: ''
-        }
-
-      default:
-        throw new Error("Unknown action")
+  let todos = [
+    {
+      id: 1,
+      text: "Не главное с какой скоростью ты двигаешься, самое главное - не останавливайся! Ежжи",
+      completed: false
     }
-  }
+  ]
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem("todos")) || todos)
 
-  const handleSubmit = event => {
-    event.preventDefault()
+  useEffect( () => {
+    localStorage.setItem("todos", JSON.stringify(state))
+  }, [state] )
 
-    alert("Отправлено сообщение" + state.text + "\n От - " + state.name)
+  const actions = useMemo( () => ({
+    addTodo: text => {
+      dispatch({ type: "ADD-TODO", payload: text })
+    },
+    toggleComplete: ( id, completed ) => {
+      dispatch({type: "TOGGLE-COMPLETE", payload: {id, completed}})
+    },
+    remove: id => {
+      dispatch({ type: "REMOVE-TODO", payload: id })
+    }
+  }), [])
 
-    dispatch({ type: "clear-state" })
-  }
-
-  const handleChange = event => {
-    dispatch({ type: "change-" + event.target.name, payload: event.target.value })
-  }
+  useEffect( () => {
+    console.log(state)
+  }, [state] )
 
   return (
-    <div className="App">
-
-      <form onSubmit={handleSubmit}>
-        <label>Имя:
-          <input type="text" name="name" value={state.name} onChange={handleChange} />
-        </label>
-
-        <label>Текст:
-          <input type="text" name="text" value={state.text} onChange={handleChange} />
-        </label>
-
-        <button type="submit" >Отправить</button>
-
-      </form>
-
-    </div>
+    <Context.Provider value={{ actions }}>
+      <div className="wrapper">
+        <TodoCreate />
+        { state.length ? <TodoList todos={ state } /> : <p>Нет задач</p> }
+      </div>
+    </Context.Provider>
   );
 }
 
